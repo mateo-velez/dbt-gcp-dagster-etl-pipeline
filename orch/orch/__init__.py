77282f5a5e7f5bc55465d4b71676c84c7e99823e
Mateo Velez <mateo.velezcobian@gmail.com>
2024-05-02 17:20:05 -0600

    Add resources to __init__.py dagster

--- orch/orch/__init__.py
+++ orch/orch/__init__.py
@@ -1,4 +1,10 @@
 from dagster import Definitions, load_assets_from_modules
+from .config.constants import RAW_BUCKET_NAME,STAGING_BUCKET_NAME,DBT_DATASET_ID
+from .config.dataproc_config import dataproc_create_cluster_config as cluster_config
+from .resources.BigQueryDataset import BigQueryDatasetResource
+from .resources.DataProcClusterResource import DataprocResource
+from .resources.ObjectStorageResource import ObjectStorageResource
+from .resources.AlbionAPIResource import AlbionAPIResource
 from .assets import dbt
 
 all_assets = load_assets_from_modules([dbt])
@@ -6,6 +12,11 @@ all_assets = load_assets_from_modules([dbt])
 defs = Definitions(
     assets=all_assets,
     resources={
-       "dbt":dbt.DbtCliResource(project_dir="dbt_assets")
+        "raw":ObjectStorageResource(bucket_name=RAW_BUCKET_NAME),
+        "staging":ObjectStorageResource(bucket_name=STAGING_BUCKET_NAME),
+        "spark_cluster":DataprocResource(cluster_config_dict=cluster_config),
+        "dbt_dataset":BigQueryDatasetResource(dataset_id=DBT_DATASET_ID),
+        "albion_api":AlbionAPIResource(host="www.albion-online-data.com"),
+        "dbt":dbt.DbtCliResource(project_dir="dbt_assets")
     }
 )
